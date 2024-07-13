6706e727d2a35331a2a0132efc6f40df40573a75
Alexander Alyonushka <sashaalenushka@gmail.com>
2024-07-13 19:49:16 +0400

    Add DataItem class implementation

--- src/main/java/DataItem.java
+++ src/main/java/DataItem.java
@@ -1,2 +1,12 @@
 public class DataItem {
+    //ключ
+    private int iData;
+
+    public DataItem(int i) {
+        this.iData = i;
+    }
+
+    public int getKey() {
+        return iData;
+    }
 }
