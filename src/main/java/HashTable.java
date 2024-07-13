e68296d51ac949d409d0ca4b0e83cb8c12e06972
Alexander Alyonushka <sashaalenushka@gmail.com>
2024-07-13 19:27:44 +0400

    Add basic insert method implementation for HashMap

---
+++ src/main/java/HashTable.java
@@ -0,0 +1,26 @@
+public class HashTable {
+
+    //Неплохо еще проверять, чтобы массив не был заполнен, иначе цикл будет вечным
+    public DataItem find(int key) {
+        int hashVal = hashFunc(key);
+
+        while(hashArray[hashVal] != null) {
+            if(hashArray[hashVal].getKey() == key)
+                return hashArray[hashVal];
+            ++hashVal;
+            hashVal %= arraySize;
+        }
+        return null;
+    }
+
+    public void insert(DataItem item) {
+        int key = item.getKey();
+        int hashVal = hashFunc(key);
+
+        while(hashArray[hashVal] != null && hashArray[hashVal].iData != -1) {
+            ++hashVal;
+            hashVal %= arraySize;
+        }
+        hashArray[hashVal] = item;
+    }
+}
