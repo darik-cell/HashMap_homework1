0bbb3e1eb143fb2fca040476bdec3f1e5d220837
Alexander Alyonushka <sashaalenushka@gmail.com>
2024-07-14 19:35:40 +0400

    Write implementation Node class

---
+++ src/main/java/Node.java
@@ -0,0 +1,57 @@
+import java.util.Map;
+import java.util.Objects;
+
+public class Node<K, V> implements Map.Entry<K, V> {
+    private int hash;
+    private final K key;
+    private V value;
+    private Node<K, V> next;
+
+    public Node(K key, V value, Node<K, V> next, int hash) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+        this.hash = hash;
+    }
+
+    public Node(Map.Entry<K, V> entry) {
+        this.key = entry.getKey();
+        this.value = entry.getValue();
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public V setValue(V value) {
+        V oldValue = this.value;
+        this.value = value;
+        return oldValue;
+    }
+
+    public Node<K, V> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<K, V> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return key + "=" + value;
+    }
+
+    public final boolean equals(Object o) {
+        if (o == this)
+            return true;
+
+        return o instanceof Map.Entry<?, ?> e
+                && Objects.equals(key, e.getKey())
+                && Objects.equals(value, e.getValue());
+    }
+}
